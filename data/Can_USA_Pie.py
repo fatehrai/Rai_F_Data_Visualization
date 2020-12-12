5dc2c3edb86182ac9100ce760ed0a1ca34a06651
fatehrai <rai.fateh30@gmail.com>
2020-12-12 03:25:14 -0500

    Added title padding

--- data/Can_USA_Pie.py
+++ data/Can_USA_Pie.py
@@ -6,6 +6,6 @@ colors = ["#ff0000", "#002868"]
 
 
 plt.pie(values, labels=labels, colors=colors, autopct = '%2.1f%%')
-plt.title("Canada & USA Total Medal Comparison")
+plt.title("Canada & USA Total Medal Comparison", pad=20)
 # generate pie chart
 plt.show()
--- data/Germany.py
+++ data/Germany.py
@@ -6,8 +6,8 @@ y = np.array([653,625,457,440,434,627])
 
 
 
-plt.bar(x, y, width = 0.5)
+plt.bar(x, y, width = 0.5,color=['gray', 'gray', 'gray', 'gray', 'gray', 'black'])
 plt.xlabel("Country")
 plt.ylabel("Number of Medals")
-plt.title("Germany Medal Total Compared To Top Five Countries With Highest Medal Totals")
+plt.title("Germany Medal Total Compared To Top Five Countries With Highest Medal Totals", pad=20)
 plt.show()
